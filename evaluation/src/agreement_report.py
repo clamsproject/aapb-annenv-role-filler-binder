6c2e93244d9a6dc5c0ff3295662f0bd5a50f71b5
Dean Cahill <dpcahill98@gmail.com>
2023-11-27 14:29:43 -0500

    Refactor and Cleanup

--- evaluation/src/agreement_report.py
+++ evaluation/src/agreement_report.py
@@ -1,12 +1,21 @@
-# Imports 
-from collections import namedtuple, defaultdict
+# Role-Filler Binding
+# Agreement Report
+"""
+  Aggregates document level agreement into 
+  human-readable metrics.
+"""
+
+# ====================================|
+# Imports
+import argparse
+from collections import defaultdict
 import csv
 import os
 import json 
+# ====================================|
 
-
-def read_in_results(fname: str) -> dict:
-
+def read_in_results(fname):
+    """Read results in as a dictionary"""
     out= {}
     with open(fname, 'r', encoding='utf8') as f:
         reader = csv.reader(f)
@@ -16,11 +25,20 @@ def read_in_results(fname: str) -> dict:
     return out
 
 def get_metrics(results_dict):
+    """Calculate the aggregation metrics
+
+    ## Metrics
+    - Zero Agreement    : number of documents which have 0.0 agreement
+    - Perfect Agreement : number of documents which have 1.0 agreement
+    - Averages          : average agreement for each locus of comparison
+        - k_total : key agreement average
+        - v_total : value agreement average
+        - p_total : pair agreement average
+        - t_total : total agreement average
+    """
+
     output_dict={}
-    metrics = ["zero_agreement",
-               "perfect_agreement",
-               "averages"]
-    
+
     num_docs = len(results_dict)
     values = defaultdict(float)
 
@@ -45,27 +63,25 @@ def get_metrics(results_dict):
     return output_dict
 
 
-
-
 def process_file(fname):
+    """Process a single file into metrics"""
     results = get_metrics(read_in_results(f"./results/noskips/{fname}"))
     print(results)
     fname = os.path.basename(fname)
     with open(f"{fname}_agreement_report.json", 'w', encoding='utf8') as f:
         json.dump(results, f, indent=2)
 
-def main():
-    fnames = ["product-nofuzzy-results.csv",
-              "product-simplefuzzy-results.csv",
-              "average-nofuzzy-results.csv",
-              "average-simplefuzzy-results.csv"]
 
-    for name in fnames:
+def main(args):
+    results_dir = args.in_dir
+    for name in os.listdir(results_dir):
         process_file(name)
 
 
-
-
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--in_dir",
+                        help="directory containing results",
+                        default="./results/")
+    runtime_args = parser.parse_args()
+    main(runtime_args)
