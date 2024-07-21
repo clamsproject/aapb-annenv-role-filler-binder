2f18a9911edb004d852adbb6b7a8a4c3d3460f1b
Keigh Rim <keigh.rim@gmail.com>
2024-07-21 15:30:57 -0400

    llm-silver: added paths caching when adding paths using baapb-resovler

--- llm-silver-anno/utils/get_paths.py
+++ llm-silver-anno/utils/get_paths.py
@@ -1,26 +1,37 @@
+from pathlib import Path
 import pandas as pd
 import requests
 from tqdm import tqdm
 import argparse
 
+cached_paths = {}
+for prev_csv in Path(__file__).parent.parent.glob("*paths.csv"):
+    df = pd.read_csv(prev_csv)
+    cached_paths.update(pd.Series(df.path.values, index=df.guid).to_dict())
+
+
 def get_full_path(guid, url):
-   full_url = f"{url}/searchapi?file=video&guid={guid}"
-   response = requests.get(full_url)
-   data = response.json()
-   return data[0]
+    if guid in cached_paths:
+        return cached_paths[guid]
+    full_url = f"{url}/searchapi?file=video&guid={guid}"
+    response = requests.get(full_url)
+    data = response.json()
+    return data[0]
+
 
 def process_data(input_file, output_file, url):
-   tqdm.pandas()
+    tqdm.pandas()
 
-   df = pd.read_csv(input_file)
-   df["path"] = df["guid"].progress_apply(get_full_path, args=(url,))
-   df.to_csv(output_file, index=False)
+    df = pd.read_csv(input_file)
+    df["path"] = df["guid"].progress_apply(get_full_path, args=(url,))
+    df.to_csv(output_file, index=False)
+
 
 if __name__ == "__main__":
-   parser = argparse.ArgumentParser(description="Process data")
-   parser.add_argument("--input_file", required=True, help="Input CSV file path")
-   parser.add_argument("--output_file", required=True, help="Output CSV file path")
-   parser.add_argument("--url", required=True, help="URL (including port) for the search API")
-   args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Process data")
+    parser.add_argument("--input", required=True, help="Input CSV file path")
+    parser.add_argument("--output", required=True, help="Output CSV file path")
+    parser.add_argument("--url", required=True, help="URL (including port) for the search API")
+    args = parser.parse_args()
 
-   process_data(args.input_file, args.output_file, args.url)
+    process_data(args.input, args.output, args.url)
